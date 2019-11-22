9e4ef1d18ab7ff62198123befe089b55915267c9
Andressa Farias <andressa.farias@compasso.com.br>
2019-11-22 14:40:11 -0300

    edição documentação script shell

--- Shell/verifyCert.sh
+++ Shell/verifyCert.sh
@@ -1,18 +1,31 @@
-#!/bi:wq
-echo 'fazendo'
+#1/bin/bash
+  
+echo ' --- Scrip para verificar a validade do certificado do dominio informado ---'
 
-echo openssl s_client -connect $1:443 2>/dev/null | openssl x509  -enddate | grep notAfter
+echo " " 
 
-echo 'feito'
-# echo | openssl s_client -connect $1:443 2>/dev/null | openssl x509  -enddate | grep notAfter
+echo Hoje é dia $(date +"%d %b %Y")
 
-#echo | openssl s_client -connect www.google.com.br:443 2>/dev/null | openssl x509  -enddate | grep notAfter
 
-# echo | openssl s_client -connect www.google.com.br:443 2>/dev/null | openssl x509  -dates | grep notAfter
+# Obtém a data de expiração doo certificado do dominio informado
 
-# echo | openssl s_client -servername unix.stackexchange.com -connect unix.stackexchange.com:443 2>/dev/null | openssl x509  -dates | grep notAfter
+validadeCertificado=$(openssl s_client -connect $1:443 2>/dev/null | openssl x509 -noout -dates | tail -1 | tr -s " " |cut -d "=" -f2 | cut -d " " -f1,2,4)
 
-# echo | openssl s_client -servername unix.stackexchange.com -connect unix.stackexchange.com:443 2>/dev/null | openssl x509 -noout -issuer -subject -dates
 
-echo | openssl s_client -connect www.google.com.br:443 2>/dev/null | openssl x509 -noout -dates | tail -1 | tr -s " " |cut -d "=" -f2 | cut -d " " -f1,2,4
+echo O certificado do dominio $1, é válido até $validadeCertificado
 
+# Transforma a data corrente em segundo:
+dataCorrenteSegundos=$(date +%s)
+
+# Transforma a String com a data de expiração em data e tranforma em segundos
+dataValidadeCertificadoSegundos=$(date -d "$validadeCertificado" +%s)
+
+# Faz obtém  diferença entre as duas datas, atual e de espiração do certficado
+
+echo " "
+
+echo Faltam  $(( (dataValidadeCertificadoSegundos  - dataCorrenteSegundos) / 86400 )) dias para o certificao expirar.
+
+
+
+#echo faltam $($validadeCertificado-$dataCorrente) dias para o certificado expirar
